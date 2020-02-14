73bfa1f30ae3141cfa3d327311d3ec3f983e1f33
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-13 19:05:52 -0500

    commit

--- lib/cricket_database/list.rb
+++ lib/cricket_database/list.rb
@@ -31,15 +31,7 @@ class List
                 @@all_stus << new_team
                 print
             elsif input == "save"
-                standing_data = File.open("CWC_Standings.txt", "w")
-                position = 1
-                standing_data.puts "2019 Cricket World Cup Standings\n\n"
-                @@all_stus.each do |team_class|
-                    standing_data.puts "#{position}: #{team_class.country} (#{team_class.initials}) \n     Name: #{team_class.country} \n     Standing: #{team_class.rank} \n     Total Matches: #{team_class.matches} \n     Total Wins: #{team_class.won} \n     Total Loses: #{team_class.lost} \n     Toatl Draw: #{team_class.draw} \n     Total Points: #{team_class.points}\n\n"
-                    position += 1
-                end
-                standing_data.close
-                puts "Successfully Saved CWC Standings"
+                
             elsif input == "exit"
                 check = true
                 goodbye
@@ -57,6 +49,18 @@ class List
                 end
             end
         end
+    end
+
+    def save
+        standing_data = File.open("CWC_Standings.txt", "w")
+        position = 1
+        standing_data.puts "2019 Cricket World Cup Standings\n\n"
+        @@all_stus.each do |team_class|
+            standing_data.puts "#{position}: #{team_class.country} (#{team_class.initials}) \n     Name: #{team_class.country} \n     Standing: #{team_class.rank} \n     Total Matches: #{team_class.matches} \n     Total Wins: #{team_class.won} \n     Total Loses: #{team_class.lost} \n     Toatl Draw: #{team_class.draw} \n     Total Points: #{team_class.points}\n\n"
+            position += 1
+        end
+        standing_data.close
+        puts "Successfully Saved CWC Standings"
     end
 
     def print
